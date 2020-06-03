9c680a77a790b5ec9f95f829f2668d3c19db2db3
Steven Vique <jhon.vique@clickonplans.com>
2020-06-02 21:39:20 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Test app barbaros
                 </div>
 
                 <div class="links">
