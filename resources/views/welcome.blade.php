028777a129bd5415c5886f9c42ab2923ecabe31d
Steven Vique <jhon.vique@clickonplans.com>
2020-06-02 21:44:22 -0500

    changes to test view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test app barbaros
+                    Test app barbaros changes
                 </div>
 
                 <div class="links">
