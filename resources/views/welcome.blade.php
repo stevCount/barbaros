a112e0ebff74d4dc4b619180b240a08090f0fa97
Steven Vique <jhon.vique@clickonplans.com>
2020-06-03 08:27:24 -0500

    changes to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test app barbaros changes.
+                    Appbarbaros
                 </div>
 
                 <div class="links">
